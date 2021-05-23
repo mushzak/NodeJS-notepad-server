8d13861ff321e186944126f2c435290ec83d90dc
Mushegh Zakaryan <mushzak@mail.ru>
2021-05-23 13:53:54 +0400

    CRUD operations are created (partly), notes manager is created to handle all the requests

---
+++ manager/NotesManager.js
@@ -0,0 +1,24 @@
+class NotesManager {
+
+    constructor() {
+    }
+
+    fetch(id) {
+
+    }
+
+    fetchAll() {
+
+    }
+
+    delete(id) {
+
+    }
+
+    update(id, data) {
+
+    }
+
+}
+
+module.exports = NotesManager;
--- routes/notes.js
+++ routes/notes.js
@@ -1,24 +1,61 @@
 const express = require('express');
 const router = express.Router();
 const dbConn  = require('../conf/db');
+const moment = require("moment");
 
 router.get('/', function(req, res, next) {
-    let query = "Select * from notes";
+    let query = "select * from notes";
+    dbConn.query(query, function(err,rows) {
+        if(err) {
+            res.send({data:''});
+        } else {
+            res.send(rows);
+        }
+    });
+});
 
-    res.send('success');
+router.get('/:id', function(req, res, next) {
+    let id = req.params.id;
+    // @todo Validations.validateInteger()
+    let query = `select * from notes where id = ${id}`;
+    dbConn.query(query, function(err,row) {
+        if(err) {
+            res.status(400).send("Error occurred");
+        } else {
+            res.send(row);
+        }
+    });
 });
 
 router.post('/', function (req, res, next) {
-
+    // @todo Validate params data (title, description)
+    let now = moment().format()
+    let query = `Insert into notes (title, description, created_at, updated_at) values ('${req.body.title}', '${req.body.description}', '${now}', '${now}')`;
+    console.log(query)
+    dbConn.query(query, function(err, row) {
+        if(err) {
+            res.status(400).send("Error occurred");
+        } else {
+            res.send(row);
+        }
+    });
 })
 
-router.put('/', function (req, res, next) {
-
+router.put('/:id', function (req, res, next) {
+    let body = req.body;
+    let noteQuery = "sel"
 })
 
-router.delete('/', function (req, res, next) {
-
+router.delete('/:id', function (req, res, next) {
+    let id = req.params.id;
+    let query = `delete from notes where id = ${id}`;
+    dbConn.query(query, function(err,row) {
+        if(err) {
+            res.status(400).send("Error occurred");
+        } else {
+            res.send(true);
+        }
+    });
 })
-
 
 module.exports = router;
